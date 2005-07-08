8b228e3ac52e42c92a8ea4abe905f9acd906bf04
gcoronel <gcoronel@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2005-07-08 11:22:00 +0000

    Actualizando algunos actores

---
+++ hardware_detection/user_level/hermes/actors/usbactor.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+
+from deviceactor import DeviceActor
+
+class Actor(DeviceActor):
+    bus = 'usb'
+
+
+    def on_added(self):
+        pass
+
+
+    def  on_removed(self):
+        pass
+
--- hardware_detection/user_level/hermes/actors/usbdeviceactor.py
+++ hardware_detection/user_level/hermes/actors/usbdeviceactor.py
@@ -5,8 +5,13 @@ from deviceactor import DeviceActor
 class Actor(DeviceActor):
     bus = 'usb_device'
 
+
     def on_added(self):
-        self.msg_render.show_info("Dispositivo usb conectado")
+        try:
+            product = self.properties['usb_device.product']
+            self.msg_render.show_info("Dispositivo detectado: " + product)
+        except:
+            self.msg_render.show_info("Dispositivo usb detectado")
 
 
     def  on_removed(self):
