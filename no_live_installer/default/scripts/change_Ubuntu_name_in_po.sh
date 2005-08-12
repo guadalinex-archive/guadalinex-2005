a733aafa850971378957b7fc9825e9d0c795c77b
carranza <carranza@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2005-08-12 13:49:48 +0000

    little change in change_name script. Optimizing

--- no_live_installer/default/scripts/change_Ubuntu_name_in_po.sh
+++ no_live_installer/default/scripts/change_Ubuntu_name_in_po.sh
@@ -4,7 +4,6 @@ PROJECT="Guadalinex"
 
 [ -z "$1" ] && echo "Usage: $0 path-to-src-directory" && exit
 
-find $1 -iname \*.po -exec perl -pi -e s/Ubuntu/$PROJECT/g {} \;
-find $1 -iname \*.templates -exec perl -pi -e s/Ubuntu/$PROJECT/g {} \;
+find $1 -iregex '.*\(po\|templates\)' -exec perl -pi -e s/Ubuntu/$PROJECT/g {} \;
 
 #find $1 -name *templates* | grep -v templates.pot | grep -v S20templates | grep -v .c$ | grep -v .pl$ | grep -v po2templates | grep -v mktemplates | grep -v mk_shortlist_templates
