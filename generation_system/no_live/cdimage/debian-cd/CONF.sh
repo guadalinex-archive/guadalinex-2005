2c1ad61b20112cdb872d02e19578a01391c69222
carlospc <carlospc@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2005-09-08 14:32:50 +0000

    Load the udeb_include

--- generation_system/no_live/cdimage/debian-cd/CONF.sh
+++ generation_system/no_live/cdimage/debian-cd/CONF.sh
@@ -329,7 +329,7 @@ export PUBLISH_PATH="/home/jigdo-area/"
 # first stage installer (debian-installer). One package per line.
 # Lines starting with '#' are comments.  The package order is
 # important, as the packages will be installed in the given order.
-#export UDEB_INCLUDE="$BASEDIR"/data/$CODENAME/udeb_include
+export UDEB_INCLUDE="$BASEDIR"/data/$CODENAME/udeb_include
 
 # File with list of packages to exclude as above.
 #export UDEB_EXCLUDE="$BASEDIR"/data/$CODENAME/udeb_exclude
