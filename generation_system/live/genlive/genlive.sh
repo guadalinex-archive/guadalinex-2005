f473de4bad28e08a436f1c84ee6fdfdc940eecea
juanje <juanje@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2005-10-25 07:04:00 +0000

    Added package for creating live systems isos

---
+++ generation_system/live/genlive/genlive.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# FIXME: very initial version, needs a lot of comprobations and so on.
+
+KERNEL="2.6.12-9-386"
+SOURCES="/media/distro/sources"
+MASTER="/media/distro/master"
+ISOS="/media/distro/isos"
+LOG_FILE="/var/log/installer.log"
+
+if [ ! -d ${MASTER}/isolinux ]; then
+	mkdir -p ${MASTER}/isolinux
+fi
+
+if [ ! -d ${MASTER}/META ]; then
+	mkdir -p ${MASTER}/META
+fi
+
+cp /usr/lib/syslinux/isolinux.bin ${MASTER}/isolinux/
+cp /usr/share/genlive/isolinux.cfg ${MASTER}/isolinux/
+cp /boot/vmlinuz-${KERNEL} ${MASTER}/isolinux/vmlinuz
+mkinitramfs -o ${MASTER}/isolinux/initramfs ${KERNEL}
+mksquashfs ${SOURCES} ${MASTER}/META/META.squashfs
+
+mkisofs -l -r -J -V "Guadalinex Live System" -hide-rr-moved -v -b isolinux/isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ${ISOS}/gl2005-$(date +%Y%m%d%H%M).iso ${MASTER}
