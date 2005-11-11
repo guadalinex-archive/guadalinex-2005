9cfa328305cd413c251e387739177d6c0f287b78
grodriguez <grodriguez@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2005-11-11 12:55:33 +0000

    Script to install Macromedia Flash plugin for Firefox

---
+++ apps_sel_and_conf/cdd/conf/2005-escritorio/internet/skel/usr/bin/instala_plugins.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+dpkg -l flashplugin-nonfree 2>&1 > /dev/null
+
+if [ `dpkg --get-selections flashplugin-nonfree|awk '{ print $2 }'` != "install" ]; then
+
+zenity --question --title "Instalación de Macromedia Flash" --text "Se va a proceder a instalar los plugins para Mozilla Firefox de Macromedia Flash. Es necesario que esté conectado a Internet.
+
+¿Desea continuar?" && gnome-terminal --hide-menubar -x gksu 'yes yes | DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get --yes install flashplugin-nonfree | cat'
+
+else
+
+zenity --info --title "Instalación de Macromedia Flash" --text "Ya tiene instalado el plugin de Macromedia Flash para Mozilla Firefox."
+
+fi
