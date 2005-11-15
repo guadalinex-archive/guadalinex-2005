a9476752d8c46eb482bbb286ba4cd8f9e9b33f96
gcoronel <gcoronel@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2005-11-15 15:48:03 +0000

    Usability features added

--- other_apps/lemurae/Main.cs
+++ other_apps/lemurae/Main.cs
@@ -3,28 +3,31 @@ using System;
 using Gtk;
 using Gdk;
 using Gecko;
-using Gnome;
 using Glade;
+
 //using GNU.Gettext;
 //using GNU.Gettext;
 
-public class DicRae: Gnome.Program
+public class DicRae
 {
 
 		[Widget] private Gtk.Entry Palabra;
-		[Widget] private Gtk.Viewport Marco; 
-		[Widget] private Gnome.App appw;  
+		[Widget] private Gtk.VBox Marco; 
+		[Widget] private Gtk.Window appw;  
+		[Widget] private Gtk.Button bAtras;
+		[Widget] private Gtk.Button bAdelante;
+		[Widget] private Gtk.Statusbar statusbar;
 		
 		private Gecko.WebControl web;
 		
         public static void Main (string[] args)
         {
-               DicRae app= new DicRae (args);               
-               app.Run();
+        	Application.Init();
+            DicRae dr = new DicRae (args);               
+            Application.Run();
         }
 
-        public DicRae (string[] args) 
-        : base ("LemuRae" , "0.1", Modules.UI, args)
+        public DicRae (string[] args)        
         {
                 LoadWidgets();                
         }
@@ -34,10 +37,15 @@ public class DicRae: Gnome.Program
 				Glade.XML gxml = new Glade.XML(null,"dicrae.glade", "appw", null);
                 gxml.Autoconnect (this);
                 
+                bAtras.Sensitive = false;
+                bAdelante.Sensitive = false;
+                
                 web=new WebControl();
                 web.Show();      
                 web.NetStop += on_net_stop;
-                          
+                web.NetStart += on_net_start;
+                statusbar.Push(0, "Listo.");
+                                          
                 Marco.Add(web);
                 Pixbuf image = new Pixbuf("/usr/share/pixmaps/lemurae.svg");
                 appw.Icon = image;                
@@ -45,20 +53,29 @@ public class DicRae: Gnome.Program
 				    
 		}
         /* Connect the Signals defined in Glade */
+        public void on_net_start(object o, EventArgs args)
+        {
+        	statusbar.Push(0, "Buscando ...");
+        }
         public void on_net_stop(object o, EventArgs args)
         {          
-          appw.Focus = Palabra;
-        }
+          	statusbar.Pop(0);          
+          	bAtras.Sensitive = web.CanGoBack();
+          	bAdelante.Sensitive = web.CanGoForward();          
+          	appw.Focus = Palabra;
+        }       
+       
         public void on_appw_delete_event (object o, DeleteEventArgs args) 
         {
-                Application.Quit ();
-                args.RetVal = true;
+            Application.Quit();
+            args.RetVal = true;
         }
         
         private void on_bBuscar_clicked (object o, EventArgs args)
-        {
+        {        	        	
+        	
         	web.LoadUrl("http://buscon.rae.es/draeI/SrvltGUIBusUsual?LEMA=" 
-        			+ Palabra.Text.ToLower());
+        			+ Palabra.Text.ToLower());            
         	Palabra.SelectRegion(0,Palabra.Text.Length);
         	appw.Focus = Palabra;
         	
