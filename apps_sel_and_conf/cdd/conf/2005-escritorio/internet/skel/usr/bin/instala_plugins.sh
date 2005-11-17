29bd3594d823f39c68b3301f07ba7aa337d22c14
grodriguez <grodriguez@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2005-11-17 14:53:38 +0000

    Fixing bad syntax

--- apps_sel_and_conf/cdd/conf/2005-escritorio/internet/skel/usr/bin/instala_plugins.sh
+++ apps_sel_and_conf/cdd/conf/2005-escritorio/internet/skel/usr/bin/instala_plugins.sh
@@ -2,11 +2,11 @@
 
 dpkg -l flashplugin-nonfree 2>&1 > /dev/null
 
-if [ `dpkg --get-selections flashplugin-nonfree|awk '{ print $2 }'` != "install" ]; then
+if [[ `dpkg --get-selections flashplugin-nonfree|awk '{ print $2 }'` != "install" ]]; then
 
 zenity --question --title "Instalación de Macromedia Flash" --text "Se va a proceder a instalar los plugins para Mozilla Firefox de Macromedia Flash. Es necesario que esté conectado a Internet.
 
-¿Desea continuar?" && gnome-terminal --hide-menubar -x gksu 'yes yes | DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get --yes install flashplugin-nonfree | cat'
+¿Desea continuar?" && gnome-terminal --hide-menubar -x gksudo 'DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get --yes install flashplugin-nonfree'
 
 else
 
