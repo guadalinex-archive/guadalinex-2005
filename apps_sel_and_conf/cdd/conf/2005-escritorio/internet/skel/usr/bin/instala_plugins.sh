b29ddd61a6f7131394ecc1040ca49a152c567134
grodriguez <grodriguez@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2005-11-18 16:21:11 +0000

    Now with gksudo goodness

--- apps_sel_and_conf/cdd/conf/2005-escritorio/internet/skel/usr/bin/instala_plugins.sh
+++ apps_sel_and_conf/cdd/conf/2005-escritorio/internet/skel/usr/bin/instala_plugins.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-dpkg -l flashplugin-nonfree 2>&1 > /dev/null
+if [[ $UID -eq 0 ]];then
 
 if [[ `dpkg --get-selections flashplugin-nonfree|awk '{ print $2 }'` != "install" ]]; then
 
-zenity --question --title "Instalación de Macromedia Flash" --text "Se va a proceder a instalar los plugins para Mozilla Firefox de Macromedia Flash. Es necesario que esté conectado a Internet.
+zenity --question --title "Instalación de Macromedia Flash" --text "Se va a proceder a instalar los plugins para Mozilla Firefox de Macromedia Flash. Es necesario que esté conectado a Interet.
 
-¿Desea continuar?" && sudo sh -c 'echo "flashplugin-nonfree install" | synaptic --hide-main-window --non-interactive --set-selections --progress-str "Instalando Macromedia Flash" --finish-str "Paquete instalado"'
+¿Desea continuar?" && echo "flashplugin-nonfree install" | synaptic --hide-main-window --non-interactive --set-selections --progress-str "Instalando Macromedia Flash" --finish-str "Paquete instalado"
 
 else
 
 zenity --info --title "Instalación de Macromedia Flash" --text "Ya tiene instalado el plugin de Macromedia Flash para Mozilla Firefox."
+
+fi
+
+else
+
+gksudo $0
 
 fi
