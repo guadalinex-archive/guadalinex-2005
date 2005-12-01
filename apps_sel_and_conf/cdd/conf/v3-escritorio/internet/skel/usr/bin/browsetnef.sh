79a7ffb35e964be331b94830cec505462505c744
grodriguez <grodriguez@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2005-12-01 18:04:32 +0000

    Mimetype for winmail.dat files, script to browse them and association between the MIME and the script

---
+++ apps_sel_and_conf/cdd/conf/v3-escritorio/internet/skel/usr/bin/browsetnef.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# wrapper script to unpackage winmail.dat
+
+UNPACKTO=/tmp/tnef  # Warning: the contents of this directory will be deleted at the end of the script
+TNEF=`which tnef`
+FILEBROWSER=`which nautilus`
+FILE=$1
+
+if [[ ! -d $UNPACKTO ]];then
+    mkdir $UNPACKTO
+fi
+
+$TNEF --file=$FILE --directory=$UNPACKTO --number-backups
+
+$FILEBROWSER file:$UNPACKTO 
+
+rm -rf $UNPACKTO/*
