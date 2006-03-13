5e5dfd1b2e0240f76e6693324d89abcc6fe90f27
carlospc <carlospc@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2006-03-13 18:29:14 +0000

    * Removing hardware_detection dir, already moved

--- hardware_detection/user_level/hermes/tests/dev_bluetooth.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-PROPERTIES = {
-    'info.category':'bluetooth_hci'
-}
--- hardware_detection/user_level/hermes/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from devicelist import *
-from coldpluglistener import *
-from captureloggui import *
-from sudo import *
-from grepmap import *
