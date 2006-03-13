993a571d7739a521bece9dcbc20f069be34a9f93
carlospc <carlospc@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2006-03-13 17:15:31 +0000

    * Adding apps_sel_and_conf and translations dirs

---
+++ trunk/apps_sel_and_conf/cdd/conf/v3-escritorio/internet/skel/etc/mozilla-firefox/pref/firefox.js
@@ -0,0 +1,34 @@
+// This is the Debian specific preferences file for Mozilla Firefox
+// You can make any change in here, it is the purpose of this file.
+// You can, with this file and all files present in the
+// /etc/mozilla-firefox/pref directory, override any preference that is
+// present in /usr/lib/mozilla-firefox/defaults/pref directory.
+// While your changes will be kept on upgrade if you modify files in
+// /etc/mozilla-firefox/pref, please note that they won't be kept if you
+// do them in /usr/lib/mozilla-firefox/defaults/pref.
+
+pref("extensions.update.enabled", true);
+pref("extensions.update.autoUpdateEnabled", false);
+pref("extensions.update.autoUpdate", false);
+
+// Use LANG environment variable to choose locale
+pref("intl.locale.matchOS", true);
+
+// Disable default browser checking.
+pref("browser.shell.checkDefaultBrowser", false);
+
+// Handle ed2k links
+pref("network.protocol-handler.app.ed2k", "/usr/bin/ed2k");
+pref("network.protocol-handler.external.ed2k", true);
+pref("network.protocol-handler.warn-external.ed2k", false);
+
+// Wipe out annoying security warnings
+pref("security.warn_entering_secure", false);
+pref("security.warn_leaving_secure", false);
+pref("security.warn_submit_insecure", false);
+
+// Download dir
+pref("browser.download.useDownloadDir", false);
+pref("browser.download.folderList", 1);
+pref("browser.download.dir", "~/Descargas");
+pref("browser.download.downloadDir", "~/Descargas");
