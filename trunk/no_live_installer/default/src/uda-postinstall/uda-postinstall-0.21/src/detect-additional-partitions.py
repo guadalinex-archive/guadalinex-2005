933bb1002e87e4853956d13e01be87fc37bb38c1
carlospc <carlospc@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2006-03-23 12:48:09 +0000

    - Modificado el backend sfdisk -> parted - Casos de traduccion con el nuevo backend:     fat32 -> vfat     linux-swap -> swap - Añadido método get_disks() - Añadido modo DEBUG - Añadido método get_partitions_already_used()     No se tocará ninguna de estas particiones, las ya existenten se quedarán comom están. Es decir, únicamente se añadirán las particiones que el usuario no haya configurado en el particionado. - passno siempre 0 para todas las particiones que no se hayan definido en el particionado

--- trunk/no_live_installer/default/src/uda-postinstall/uda-postinstall-0.21/src/detect-additional-partitions.py
+++ trunk/no_live_installer/default/src/uda-postinstall/uda-postinstall-0.21/src/detect-additional-partitions.py
@@ -1,4 +1,19 @@
+# -*- coding: utf-8 -*-
+
 import os
+
+# Changelog
+# - Modificado el backend sfdisk -> parted
+# - Casos de traduccion con el nuevo backend:
+#     fat32 -> vfat
+#     linux-swap -> swap
+# - Añadido método get_disks()
+# - Añadido modo DEBUG 
+# - Añadido método get_partitions_already_used()
+#   No se tocará ninguna de estas particiones, las ya existenten se quedarán comom están. Es decir, únicamente se añadirán las particiones que el usuario no haya configurado en el particionado.
+# - passno siempre 0 para todas las particiones que no se hayan definido en el particionado
+
+DEBUG=0
 
 def get_partitions():
   """returns an array with fdisk output related to partition data."""
@@ -13,6 +28,20 @@ def get_partitions():
 
   return partition
 
+def get_disks():
+  """returns a set with the disks detected."""
+
+  import re
+
+  # parsing partitions from the procfs
+  # attetion with the output format. the partitions list is without '/dev/'
+  partition_table = open('/proc/partitions').read()
+  regex = re.compile('[sh]d[a-g]')
+  disks = regex.findall(partition_table)
+  disks = set(disks)
+
+  return disks
+
 def get_filesystems():
   """returns a dictionary with a skeleton { device : filesystem }
   with data from local hard disks. Only swap and ext3 filesystems
@@ -21,76 +50,110 @@ def get_filesystems():
   import re, subprocess
   device_list = {}
 
-  partition_list = get_partitions()
-  for device in partition_list:
-    device = '/dev/' + device
-    filesystem_pipe = subprocess.Popen(['sfdisk', '-c', device[0:8], device[8:]], stdout=subprocess.PIPE)
+  disks_list = get_disks()
 
-    filesystem = filesystem_pipe.communicate()[0]
-    if '83' in filesystem:
-      device_list[device] = 'ext3'
-    elif '82' in filesystem:
-      device_list[device] = 'swap'
-    elif 'b' in filesystem or 'c' in filesystem:
-      device_list[device] = 'vfat'
-    elif '7' in filesystem:
-      device_list[device] = 'ntfs'
+  if DEBUG:
+    print "disk_list: ", disks_list
+
+  # For each disk, have a look for the partitions that it has.
+  for disk in disks_list:
+    disk = '/dev/' + disk
+    if DEBUG:
+      print "disk: " + disk
+    partition_pipe = subprocess.Popen(['parted', '-s', disk, 'print'], stdout=subprocess.PIPE)
+    partitions = partition_pipe.communicate()[0]
+
+    partitions = partitions.splitlines()
+    # If the output is smaller than this, is not correct.
+    if partitions.__len__() > 2:
+      # For each line of partition... the first partition starts at the 4th line
+      for x in range(3,partitions.__len__()):
+        temp_line = partitions[x].split()
+        # This condiction has to be satisfied if parted has identified the partition fs.
+        if temp_line.__len__() == 5:
+          partition_number = temp_line[0]
+          partition_type = temp_line[4]
+	  if partition_type == 'fat32':
+            device_list[disk + partition_number] = 'vfat'
+          elif partition_type == 'linux-swap':
+            device_list[disk + partition_number] = 'swap'
+          else:
+            device_list[disk + partition_number] = partition_type
+          if DEBUG:
+            print "partition: ", partition_number, partition_type, disk + partition_number, device_list[disk + partition_number]
+         
   return device_list
 
-
-fstab = open('/etc/fstab', 'r')
-blacklist = []
-import re
-for line in fstab.readlines():
-	list_columns = line.split()
-	try:
-		if list_columns[1] == '/' or list_columns[1] == '/home':
-			blacklist.append(list_columns[0])
-		if list_columns[1] == 'none' and list_columns[2] == 'swap':
-			blacklist.append(list_columns[0])
-                homing = re.compile('/media/*')
-                is_true = homing.match(list_columns[1])
+def get_partitions_already_used():
+  """returns an array containing all the partitions already listed."""
+  fstab = open('/etc/fstab', 'r')
+  blacklist = []
+  import re
+  for line in fstab.readlines():
+  	list_columns = line.split()
+  	try:
+  		dev_re = re.compile('/dev/*')
+  		is_true = dev_re.match(list_columns[0])
+  
                 if is_true:
-                        blacklist.append(list_columns[0])
-	except:
-		pass
-fstab.close()
+                  blacklist.append(list_columns[0])
+  	except:
+  		pass
+  fstab.close()
+  
+  return blacklist
+
+########
+# MAIN #
+########
+
+if DEBUG:
+  print "DEBUG MODE ENABLED"
+
+blacklist = get_partitions_already_used()
+
+if DEBUG:
+  print "blacklist: ", blacklist
 
 fstab = open('/etc/fstab','a')
 
 win_counter = 1
+passno = 0
 
 for new_device, fs in get_filesystems().items():
+  # CASE 1: Windows partitions
   if ( fs in ['vfat', 'ntfs'] ):
-    passno = 2
-    if fs == 'vfat' :
-      options = 'rw,gid=100,users,umask=0002,fmask=0113,sync,noauto,defaults'
-    else:
-      options = 'gid=100,users,umask=0222,fmask=0333,sync,nls=utf8,noauto,defaults'
-    path = '/media/Windows%d' % win_counter
-    if not os.path.exists(path):
-      os.mkdir(path)
-    win_counter += 1
-    print >>fstab, '%s\t%s\t%s\t%s\t%d\t%d' % (new_device, path, fs, options, 0, passno)
-  elif fs == 'ext3':
-    passno = 2
-    if (new_device in blacklist):
-      pass
-    else:
-      options = 'defaults,users,exec,auto'
+    if not (new_device in blacklist):
+      if fs == 'vfat' :
+        options = 'rw,gid=100,users,umask=0002,fmask=0113,sync,noauto,defaults'
+      else:
+        options = 'gid=100,users,umask=0222,fmask=0333,sync,nls=utf8,noauto,defaults'
+      path = '/media/Windows%d' % win_counter
+      if not os.path.exists(path):
+        os.mkdir(path)
+      win_counter += 1
+      if DEBUG:
+        print '%s\t%s\t%s\t%s\t%d\t%d' % (new_device, path, fs, options, 0, passno)
+      else:
+        print >>fstab, '%s\t%s\t%s\t%s\t%d\t%d' % (new_device, path, fs, options, 0, passno)
+  # CASE 2: swap
+  elif fs == 'swap':
+    if not (new_device in blacklist):
+      options = 'sw'
+      path = 'none'
+      if DEBUG:
+        print '%s\t%s\t%s\t%s\t%d\t%d' % (new_device, path, fs, options, 0, passno)
+      else:
+        print >>fstab, '%s\t%s\t%s\t%s\t%d\t%d' % (new_device, path, fs, options, 0, passno)
+  # CASE 3: common partitions types that hasn't been set yet
+  elif fs in ['ext3', 'ext2', 'jfs', 'reiserfs', 'xfs']:
+    if not (new_device in blacklist):
+      options = 'defaults'
       path = '/media/%s%d' % (new_device[5:8],int(new_device[8:]))
       if not os.path.exists(path):
         os.mkdir(path)
-      print >>fstab, '%s\t%s\t%s\t%s\t%d\t%d' % (new_device, path, fs, options, 0, passno)
-  elif fs == 'swap':
-    if (new_device in blacklist):
-      pass
-    else:
-      options = 'sw'
-      path = 'none'
-      passno = 0
-      print >>fstab, '%s\t%s\t%s\t%s\t%d\t%d' % (new_device, path, fs, options, 0, passno)
-
-
-
+      if DEBUG:
+        print '%s\t%s\t%s\t%s\t%d\t%d' % (new_device, path, fs, options, 0, passno)
+      else:
+        print >>fstab, '%s\t%s\t%s\t%s\t%d\t%d' % (new_device, path, fs, options, 0, passno)
 fstab.close()
