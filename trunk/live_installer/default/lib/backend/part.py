9498ba87bfb9b59475b8d76e8731d0e26eca6163
juanje <juanje@083a9b74-26fa-0310-9f4a-f08d686dee1e>
2006-04-20 00:29:41 +0000

    Added notparted functions stuff

--- trunk/live_installer/default/lib/backend/part.py
+++ trunk/live_installer/default/lib/backend/part.py
@@ -53,6 +53,7 @@
 
 from subprocess import *
 from os import system
+import misc
 
 def call_autoparted (assistant, drive, progress = None):
 
@@ -112,7 +113,137 @@ def extend(drive):
               if line[4] == '5':
                 return True
   return False
-  
+ 
+def limits_for_scheme(begin, end, scheme):
+    mega_scheme = {}
+    beg = begin+1
+    en  = beg+scheme['root']
+    mega_scheme['root'] = [beg, en]
+    beg = en+1
+    en  = beg+scheme['home']
+    mega_scheme['home'] = [beg, en]
+    beg = en+1
+    en  = end-2
+    mega_scheme['swap'] = [beg, en]
+    return mega_scheme
+
+
+def part_scheme(drive, scheme):
+    if scheme.has_key('extended'):
+        misc.ex("parted -s %s mkpart extended %d %d" % (drive, scheme['extended'][0], scheme['extended'][1]) )
+    misc.ex("parted -s %s mkpart logical ext2 %d %d" % (drive, scheme['root'][0], scheme['root'][1]) )
+    misc.ex("parted -s %s mkpart logical ext2 %d %d" % (drive, scheme['home'][0], scheme['home'][1]) )
+    misc.ex("parted -s %s mkpart logical linux-swap %d %d" % (drive, scheme['swap'][0], scheme['swap'][1]) )
+    # TODO: Some error code handling needed with the execution of this commands
+    # above
+    return True
+
+
+def get_empty_space(drive):
+    # As sector size is supossed to be 512 the ratio MB/Sec is 1/2048
+    conv_value = 2048 
+    
+    import os
+    os.putenv('LANG','C')    
+    out = Popen(['/sbin/cfdisk', '-P', 's', drive], stdin=PIPE, stdout=PIPE, close_fds=True)
+    cont = out.stdout.readlines()
+    cont = cont[5:]
+    if extend(drive):
+        extended = None
+        frees = []
+        list = []
+        logics = 0
+        for i in cont:
+            j = i[3:]
+            j = j.replace('*',' ')
+            line = j.split()
+            if line[5] == 'Extended':
+                extended = cont.index(i)
+            elif line[5] == 'Free':
+                frees.append(cont.index(i))
+    	    elif line[0] == 'Logical':
+    	        logics += 1
+            begin, end, length = int(line[1]), int(line[2]), int(line[4])
+            list.append([begin, end, length])
+        
+        # If there is more than 12 logic partitions(4 primaries plus 8 logicals) exit
+        if logics > 8:
+    	    print "Too much logical partitions"
+    	    pre_log('error', "Too much logical partitions")
+    	    return (None,None)
+    
+        sizes = []
+        indexs = []
+        for i in list:
+            if list[extended] != i and i[-1] < list[extended][1] and i[1] > list[extended][0] and list.index(i) in frees:
+                sizes.append(i[2])
+                indexs.append(list.index(i))
+        
+        if not sizes:
+	    print "No freespace has enough room"
+	    pre_log('error', "No freespace has enough room")
+	    return (None,None)
+        bigger = max(sizes)
+        index = sizes.index(bigger)
+        i = indexs[index]
+        tam = sizes[index]/conv_value
+        scheme = calc_sizes(tam)
+        begin = list[i][0]/conv_value
+        end = list[i][1]/conv_value
+    	if not scheme:
+    	    print "Not enough space"
+    	    pre_log('error', "Not enough space")
+    	    return (None,None)
+        limits = limits_for_scheme(begin, end, scheme)
+        logic_dev = logics + 4
+        partitions = { '/'     : "%s%d" % (drive,logic_dev+1),
+                       '/home' : "%s%d" % (drive,logic_dev+2),
+                       'swap'  : "%s%d" % (drive,logic_dev+3)
+                     }
+    else:
+        nonfrees = 0
+        list = []
+        for i in cont:
+            i = i.replace('*',' ')
+    	    line = i.split()
+            # If there is not any number in the first value, it's not a
+            # partition, but it's a free space
+    	    if not line[0].isdigit():
+                begin, end, length = int(line[1]), int(line[2]), int(line[4])
+                list.append([begin, end, length])
+            else:
+                nonfrees += 1
+        if nonfrees > 3:
+            print "Too many primary partitions"
+    	    pre_log('error', "Too many primary partitions")
+    	    return (None,None)
+        sizes = []
+        for i in list:
+            sizes.append(i[2])
+        if not sizes:
+	    print "No freespace has enough room"
+	    pre_log('error', "No freespace has enough room")
+	    return (None,None)
+        bigger = max(sizes)
+        index = sizes.index(bigger)
+        tam = sizes[index]/conv_value
+        scheme = calc_sizes(tam)
+        begin = (list[index][0]/conv_value)+2
+        end = (list[index][1]/conv_value)-2
+    	if not scheme:
+    	    print "Not enough space"
+    	    pre_log('error', "Not enough space")
+    	    return (None,None)
+        limits = limits_for_scheme(begin, end, scheme)
+        limits['extended'] = [begin, end]
+        partitions = { '/'     : "%s%d" % (drive,5),
+                       '/home' : "%s%d" % (drive,6),
+                       'swap'  : "%s%d" % (drive,7) 
+                     }
+                       
+    return (limits,partitions)
+        
+ 
 def get_schemes_list(drive):
   # 1 - get the disk size
   tam = get_disk_size(drive)
